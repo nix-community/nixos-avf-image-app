b86326e649e3ee105371d2b230ac12971aaf9d60
Maciej Krüger <mkg20001@gmail.com>
2025-04-07 06:00:36 +0200

    work on install activity

--- app/src/main/java/io/mkg20001/nixosimage/Install.kt
+++ app/src/main/java/io/mkg20001/nixosimage/Install.kt
@@ -1,16 +1,40 @@
 package io.mkg20001.nixosimage
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import io.mkg20001.nixosimage.data.GitHubReleaseAsset
+import io.mkg20001.nixosimage.databinding.ActivityInstallBinding
+import io.mkg20001.nixosimage.databinding.FragmentHomeBinding
+import io.mkg20001.nixosimage.install.ImageInstallMethod
 import io.mkg20001.nixosimage.install.InstallMethods
+import io.mkg20001.nixosimage.ui.home.HomeViewModel
 
 
 class Install : AppCompatActivity() {
+    var method: ImageInstallMethod? = null
+    var asset: GitHubReleaseAsset? = null
+
+    private lateinit var binding: ActivityInstallBinding
+
+    private val _text = MutableLiveData<String>().apply {
+        value = ""
+    }
+    val text: LiveData<String> = _text
+
+    private val _progress = MutableLiveData<Int>().apply {
+        value = 0
+    }
+    val progress: LiveData<Int> = _progress
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,26 +45,46 @@ class Install : AppCompatActivity() {
             insets
         }
 
+        binding = ActivityInstallBinding.inflate(layoutInflater)
+
         val b = intent.extras ?: return errorOut()
 
-        val r: GitHubReleaseAsset = b.getSerializable("image", GitHubReleaseAsset::class.java)
+        asset = b.getSerializable("image", GitHubReleaseAsset::class.java)
             ?: return errorOut()
+
         val m: String = b.getString("method")
             ?: return errorOut()
-
-        val method = InstallMethods.getMethod(m)
+        method = InstallMethods.getMethod(m)
             ?: return errorOut()
 
-        Toast.makeText(
-            applicationContext, method.id,
-            Toast.LENGTH_LONG
-        ).show()
+        text.observeForever {
+            binding.status.text = it
+        }
+        progress.observeForever {
+            binding.progress.progress = it
+        }
 
         // TODO: downloaded = download(r.url)
         // TODO: method.installImage(downloaded)
+
+        doInstall()
+    }
+
+    fun doInstall() {
+        updateStatus(R.string.install_step_downloading)
+        _progress.postValue(20)
+    }
+
+    fun updateStatus(task: Int) {
+        val out = getString(R.string.install_task) + " " + getString(task) + "\n\n" +
+                getString(R.string.install_version) + " " + asset!!.version + "\n\n" +
+                getString(R.string.install_architecture) + " " + asset!!.arch
+
+        _text.postValue(out)
     }
 
     fun errorOut() {
         // set status to "Error"
+        _text.postValue("Error!")
     }
 }
