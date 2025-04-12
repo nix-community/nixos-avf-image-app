b29aa55c00c5116939df3505cdf2b3956c2d00f8
Maciej Krüger <mkg20001@gmail.com>
2025-04-12 22:11:53 +0200

    make it work

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
 #[macro_use] extern crate rocket;
 
-
+use std::env;
 use graphql_client::{GraphQLQuery, Response};
 use serde::{Deserialize, Serialize};
 use rocket::serde::{json::Json};
-use crate::get_releases::Variables;
+use rocket::State;
+use crate::get_releases::{Variables, ResponseData};
+use twelf::{config, Layer};
 
 #[allow(clippy::upper_case_acronyms)]
 type URI = String;
 type DateTime = String;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-struct QueryBody<'a> {
+struct QueryBody {
     /// The values for the variables. They must match those declared in the queries.
     pub variables: EmptyVars,
     /// The GraphQL query, as a string.
-    pub query: &'a str,
+    pub query: String,
     /// The GraphQL operation name, as a string.
     #[serde(rename = "operationName")]
-    pub operation_name: &'a str,
+    pub operation_name: String,
 }
 
 /*
@@ -49,13 +51,31 @@ struct EmptyVars {
 )]
 struct GetReleases;
 
+#[config]
+#[derive(Debug, Default, Serialize, Clone)]
+pub struct Config {
+    pub port: u16,
+    pub token: String,
+}
+
+pub fn loadConfig() -> Config {
+    // Layer from different sources to build configuration. Order matters!
+    let conf = Config::with_layers(&[
+        Layer::Yaml(env::current_dir().expect("Failed to get CWD").join("config.yaml")),
+        Layer::Env(Some(String::from("APP_"))),
+    ]).expect("Failed to load config");
+
+    conf
+}
+
 #[post("/graphql", data = "<query>")]
-async fn graphql(query: Json<QueryBody<'_>>) -> Json<Response<GetReleases>> {
+async fn graphql(query: Json<QueryBody>, config: &State<Config>) -> Json<Response<ResponseData>> {
     let request_body = GetReleases::build_query(Variables {});
 
-    // Send the query (e.g., using reqwest or another HTTP client)
-    let response: Response<GetReleases> = reqwest::Client::new()
-        .post("https://api.example.com/graphql")
+    let response: Response<ResponseData> = reqwest::Client::new()
+        .post("https://api.github.com/graphql")
+        .header(reqwest::header::USER_AGENT, "github.com/mkg20001/nixos-avf-image-app")
+        .bearer_auth(config.token.clone())
         .json(&request_body)
         .send()
         .await
@@ -69,5 +89,9 @@ async fn graphql(query: Json<QueryBody<'_>>) -> Json<Response<GetReleases>> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![graphql])
+    let config = loadConfig();
+    rocket::build()
+        .configure(rocket::Config::figment().merge(("port", config.port)).merge(("address", "::")))
+        .manage(config)
+        .mount("/", routes![graphql])
 }
