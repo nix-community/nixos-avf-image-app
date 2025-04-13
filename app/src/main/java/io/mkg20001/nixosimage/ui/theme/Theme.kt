8e4ffecca9f98dc5c05506245cf05dd1142b7a1e
Maciej Krüger <mkg20001@gmail.com>
2025-04-13 02:54:00 +0200

    fully switch to dynamic color

--- app/src/main/java/io/mkg20001/nixosimage/ui/theme/Theme.kt
+++ app/src/main/java/io/mkg20001/nixosimage/ui/theme/Theme.kt
@@ -1,61 +1,20 @@
 package io.mkg20001.myapplication.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import io.mkg20001.nixosimage.ui.theme.Blue500
-import io.mkg20001.nixosimage.ui.theme.Blue700
-import io.mkg20001.nixosimage.ui.theme.Indigo200
-import io.mkg20001.nixosimage.ui.theme.Indigo700
-import io.mkg20001.nixosimage.ui.theme.Pink40
-import io.mkg20001.nixosimage.ui.theme.Pink80
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Blue700,
-    onPrimary = Color.Black,
-    secondary = Indigo700,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Blue500,
-    onPrimary = Color.White,
-    secondary = Indigo200,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 @Composable
 fun NixosImageTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        else -> dynamicLightColorScheme(LocalContext.current)
     }
 
     MaterialTheme(
