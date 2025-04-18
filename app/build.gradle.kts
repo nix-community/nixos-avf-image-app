951c5d30203d883132ac3616a79b9cb37ec70aed
Maciej Krüger <mkg20001@gmail.com>
2025-04-18 17:35:47 +0200

    version 0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.mkg20001.nixosimage"
         minSdk = 35
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.2"
+        versionCode = 5
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
